98e6214d7ac89773920c0a65397f19ae72a26ed8
nohestmm <nohestmm@gmail.com>
2019-04-04 11:22:38 -0300

    Agregando validacion de minusculas

--- src/cipher.js
+++ src/cipher.js
@@ -1,32 +1,37 @@
 
 
- let x, cipher1 , deCipher , asciiCode;
+ let x, cipher1 , deCipher , asciiCode, asciiCodem;
  
  window.cipher = {
  
   
   encode: (offset,string) => {
     cipher1 = '';
+    x = 0 ;
     for(let i=0; i<string.length;i++){
 
       x = string.charCodeAt(i);
      
+
        if (x === 32 || x === 44)
         {
           cipher1 = cipher1 + String.fromCharCode(x); 
         }
-      else if(x>=65 && x<= 90)
-      {
-        asciiCode = (((x - 65) + offset) % 26) + 65; 
+
+      
+      else if(x >= 65 && x <= 90)
+      {asciiCode = (((x - 65) + offset) % 26) + 65;
+      
         cipher1 = cipher1 + String.fromCharCode(asciiCode);
       }
-     else if (x>= 97 && x<= 122)
-     {
-      asciiCode = (((x - 97) + offset) % 26) + 97; 
-      cipher1 = cipher1 + String.fromCharCode(asciiCode);
+  else if (x >= 97 && x<=122)
+  {
+    asciiCodem = (((x - 97) + offset) % 26) + 97;
+    cipher1 = cipher1 + String.fromCharCode(asciiCodem); 
 
-     }
 
+  }
+     
          
     }
       
@@ -39,24 +44,26 @@
     
     for(let i=0; i<string.length;i++){
       
-      x = string.charCodeAt(i);
+      x = string.charCodeAt(i); 
+    
+
       if (x === 32 || x === 44)
        {
          deCipher = deCipher + String.fromCharCode(x); 
        }
      else if(x>=65 && x<= 90)
-     {
-      asciiCode = (((x + 65) - offset) % 26) + 65; 
-      
-      deCipher = deCipher + String.fromCharCode(asciiCode);
+     { 
+       asciiCode = (((x + 65) - offset) % 26) + 65; 
+       deCipher = deCipher + String.fromCharCode(asciiCode);
      }
-     else if (x>= 97 && x<= 122)
+     
+     else if (x >= 97 && x<=122)
      {
-      asciiCode = (((x - 97) - offset) % 26) + 97; 
-      deCipher = deCipher + String.fromCharCode(asciiCode);
-
+       asciiCodem = (((x - 122) - offset) % 26) + 122;
+       deCipher = deCipher + String.fromCharCode(asciiCodem); 
+   
+   
      }
-
      
    }
     
