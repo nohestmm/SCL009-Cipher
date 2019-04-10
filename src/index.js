5926bde59a5f73e95f6516443049a8f02c5052b1
nohestmm <nohestmm@gmail.com>
2019-04-10 09:26:21 -0300

    responsive

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ let offset,string,returnCipher,returnDecipher, action;
 
 
 btnAccess.addEventListener("click",() => {
-  printHtml += '<div class="row"><div class="col-md-12"><img src="images/logo.jpg"></div><div class="col-md-12"><p class="instructor"><b>Instrucciones:</b><br>Colocar un desplazamiento<br>Escribir en el textarea los datos a cifrar/descifrar.<br>Pulsar boton cifrado o descifrado.<br>Visualizar resultado.<br>Limpiar campos.</p></div></div><div class="row"><div class=" col-md-12">';
+  printHtml += '<div class="row"><div class="col-12 col-md-7"><img src="images/logo.jpg"></div><div class="col-12 col-md-5"><p class="instructor"><b>Instrucciones:</b><br>Colocar un desplazamiento<br>Escribir en el textarea los datos a cifrar/descifrar.<br>Pulsar boton cifrado o descifrado.<br>Visualizar resultado.<br>Limpiar campos.</p></div></div><div class="row"><div class="col-12 col-xs-12 col-sm-12 col-md-6">';
   printHtml += '<p>Desplazamiento:<input type="number" min="1" max="100" id="offsetCipher" class="textOffset"></p><p class="Error" id="Error2">Debe ingresar un numero de desplazamiento</p></div></div>';
-  printHtml += '<div class="row"><div class="col-md-12"><p>Ingresa los datos de tu cuenta:</p><textarea id="textCipher" cols="30" rows="8" class="textUp"></textarea>';
+  printHtml += '<div class="row"><div class="col-12 col-xs-12 col-sm-12 col-md-6"><p>Ingresa los datos de tu cuenta:</p><textarea id="textCipher" cols="30" rows="8" class="textUp"></textarea>';
   printHtml += '<p class="Error" id="Error1">Debe ingresar una frase</p></div><div class="col"><p id="labelResult">Resultado:</p><div class="divResult"><p id="resultCipher"></p></div></div></div>';
   printHtml += '<div class="row"><div class="col-4"><button id="btnCipher" class="btn btn-info">Cifrar</button> </div><div class="col-4"><button class="btn btn-info" id="btnDecipher">Descifrar</button></div><div class="col-4"><button id="clean" class="btn btn-info">Limpiar</button></div></div></div><br><br><footer class="centert cursive">Todos los derechos reservados 2019</>';
   
